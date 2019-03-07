2eeb9630b28fa03fd9fd0a7be32327b0a03676ab
wfq <946586142@qq.com>
2019-03-07 17:28:24 +0800

    aidl 客户端代码 增加接口

--- app/src/main/java/www/wfq/com/aidlclient/MainActivity.java
+++ app/src/main/java/www/wfq/com/aidlclient/MainActivity.java
@@ -1,11 +1,10 @@
 package www.wfq.com.aidlclient;
 
+import android.annotation.SuppressLint;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.os.Bundle;
-import android.os.IBinder;
-import android.os.RemoteException;
+import android.os.*;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -13,6 +12,7 @@ import android.view.View;
 import android.widget.Button;
 import www.wfq.com.aidlserver.aidl.Book;
 import www.wfq.com.aidlserver.aidl.IBookController;
+import www.wfq.com.aidlserver.aidl.IOnNewBookArrivedListener;
 
 import java.util.List;
 
@@ -65,12 +65,39 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+    // Binder的死亡接收
+    private IBinder.DeathRecipient deathRecipient = new IBinder.DeathRecipient() {
+        @Override
+        public void binderDied() {
+            if (controller == null)
+                return;
+            Log.e("WFQ", "binder death");
+            // 在这里移除之前绑定的binder，然后重新绑定远程的服务
+            controller.asBinder().unlinkToDeath(deathRecipient, 0);
+            controller = null;
+            bindService();
+
+        }
+    };
+
+
     private ServiceConnection connection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
             isConnected = true;
             Log.e("WFQ客户端", "连接上服务");
             controller = IBookController.Stub.asInterface(iBinder);
+            try {
+                controller.registerListener(mOnNewBookArrivedListener);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+            try {
+                iBinder.linkToDeath(deathRecipient, 0);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
         }
 
         @Override
@@ -80,6 +107,30 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+
+    @SuppressLint("HandlerLeak")
+    private Handler mainHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+
+            switch (msg.what) {
+                case 1:
+                    Log.e("WFQ", "客户端：收到新增一本新书的通知");
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    };
+
+    private IOnNewBookArrivedListener mOnNewBookArrivedListener = new IOnNewBookArrivedListener.Stub() {
+        @Override
+        public void onNewBookArrived(Book newbook) throws RemoteException {
+            mainHandler.obtainMessage(1,newbook).sendToTarget();
+        }
+    };
+
+
     private void bindService() {
         Intent intent = new Intent();
         intent.setPackage("www.wfq.com.aidlserver");
@@ -88,6 +139,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void unBindService() {
+        if(controller != null && controller.asBinder().isBinderAlive()){
+            try {
+                controller.unregisterListener(mOnNewBookArrivedListener);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
         if (isConnected) unbindService(connection);
     }
 
